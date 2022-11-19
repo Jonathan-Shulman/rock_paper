df08d9404cfa2757d1bd7de0b9b620c69c2b9246
Jonathan <117773602+Jonathan-Shulman@users.noreply.github.com>
2022-11-19 19:03:45 +0200

    added ui to rockpapoer

--- script.js
+++ script.js
@@ -16,8 +16,13 @@ function getComputerChoice(){
     
 }
 
-function playRound(playerSelection, playerScore, computerScore){
+function playRound(playerSelection){
     let computerSelection = getComputerChoice();
+    let curscore = document.querySelector("#cur-score");
+    let cur = curscore.textContent;
+    let playerScore = cur.charAt(0)
+    let computerScore = cur.charAt(2)
+    let msg = document.querySelector('#msg')
     playerSelection = playerSelection.toUpperCase();
     computerSelection = computerSelection.toUpperCase();
     if (playerSelection == computerSelection){
@@ -30,53 +35,97 @@ function playRound(playerSelection, playerScore, computerScore){
     else if (playerSelection == "ROCK"){
         if (computerSelection == "PAPER"){
             console.log("LOST!") ;
-            computerScore= computerScore + 1;
+            computerScore= `${parseInt(computerScore)+ 1}`;
+            curscore.textContent = `${playerScore}-${computerScore}`;
             return ;
         }
         console.log("WON!");
-        playerScore= playerScore + 1;
+        playerScore= `${parseInt(playerScore)+ 1}`;
+        curscore.textContent = `${playerScore}-${computerScore}`;
         return
     }
     else if (playerSelection == "PAPER"){
         if (computerSelection == "SCISSORS"){
             console.log("LOST!");
-            computerScore= computerScore + 1;
+            computerScore= `${parseInt(computerScore)+ 1}`;
+            curscore.textContent = `${playerScore}-${computerScore}`;
+            if (playerScore=="5" || computerScore=="5"){
+                if (parseInt(playerScore)>parseInt(computerScore)){
+                    msg.textContent = 'You Lost! press "play again" to restart';
+                }
+            }
+
             return
         }
         console.log("WON!");
-        playerScore= playerScore + 1;
+        playerScore= `${parseInt(playerScore)+ 1}`;
+        curscore.textContent = `${playerScore}-${computerScore}`;
+
         return
     }
     else if (playerSelection == "SCISSORS"){
         if (computerSelection == "ROCK"){
             console.log("LOST!");
-            computerScore= computerScore + 1;
+            computerScore= `${parseInt(computerScore)+ 1}`;
+            curscore.textContent = `${playerScore}-${computerScore}`;
+            
             return
         }
         console.log("WON!");
-        playerScore= playerScore + 1;
+        playerScore= `${parseInt(playerScore)+ 1}`;
+        curscore.textContent = `${playerScore}-${computerScore}`;
         return
     }
 
 
 
 }
+const reset = document.querySelector("#play-again")
+reset.addEventListener('click', () =>{
+    let curscore = document.querySelector("#cur-score");
+    curscore.textContent = "0-0"
 
+})
 const rock_btn = document.querySelector("button#rock");
 rock_btn.addEventListener('click', function(e){
-    playRound("rock", playerScore, computerScore);
+    let computerSelection = getComputerChoice();
+    let curscore = document.querySelector("#cur-score");
+    let cur = curscore.textContent;
+    let playerScore = cur.charAt(0)
+    let computerScore = cur.charAt(2)
+    let msg = document.querySelector('#msg')
+    if (parseInt(playerScore) != 5 && parseInt(computerScore) != 5){
+        playRound("rock");
+    }
+
 
 
 });
 const paper_btn = document.querySelector("button#paper");
 paper_btn.addEventListener('click', function(e){
-    playRound("paper", playerScore, computerScore);
+    let computerSelection = getComputerChoice();
+    let curscore = document.querySelector("#cur-score");
+    let cur = curscore.textContent;
+    let playerScore = cur.charAt(0)
+    let computerScore = cur.charAt(2)
+    let msg = document.querySelector('#msg')
+    if (parseInt(playerScore) != 5 && parseInt(computerScore) != 5){
+        playRound("paper");
+    }
 
 
 });
 const scissors_btn = document.querySelector("button#scissors");
 scissors_btn.addEventListener('click', function(e){
-    playRound("scissors", playerScore, computerScore);
+    let computerSelection = getComputerChoice();
+    let curscore = document.querySelector("#cur-score");
+    let cur = curscore.textContent;
+    let playerScore = cur.charAt(0)
+    let computerScore = cur.charAt(2)
+    let msg = document.querySelector('#msg')
+    if (parseInt(playerScore) != 5 && parseInt(computerScore) != 5){
+        playRound("scissors");
+    }
 
 
 });
